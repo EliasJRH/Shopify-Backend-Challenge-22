81731fcf58807cdce69dc49b068fc4c9201e11ab
Elias Hawa <elias.hawa2468@gmail.com>
2022-01-08 14:54:19 -0500

    Add error handling

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,16 +1,29 @@
+const { query } = require("express");
 const express = require("express");
-const { route } = require("express/lib/application");
 const router = express.Router();
 const Inventory = require("../models/Inventory");
 
 router.get("/", async (req, res) => {
-  const allInventory = await Inventory.find();
-  res.status(200).send(allInventory);
+  try {
+    const allInventory = await Inventory.find(req.query);
+    res.status(200).send(allInventory);
+  } catch (err) {
+    res.status(500).json({message: err.message});
+  }
 });
 
 router.post("/", async (req, res) => {
-  const newInventory = await Inventory.create(req.body);
-  res.status(200).send("OK");
+    try{
+        const newInventory = await Inventory.create(req.body);
+        res.status(200).send(newInventory);
+    }catch(err){
+        if (err._message == "Inventory validation failed"){
+            res.status(400).send({message: err.message})
+        }else{
+            res.status(500).send({message: err.message})
+        }
+    }
+  
 });
 
 module.exports = router;
