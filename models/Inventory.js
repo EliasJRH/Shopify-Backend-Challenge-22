d7327c1c8aa503a3fad6fd19ca13d5e0084f298b
Elias Hawa <elias.hawa2468@gmail.com>
2022-01-09 18:25:43 -0500

    Add upc code to inventory model

--- models/Inventory.js
+++ models/Inventory.js
@@ -1,4 +1,62 @@
 const mongoose = require("mongoose");
+const crypto = require("crypto");
+
+const manufacturerCode = "420690";
+
+const calculateCheckSum = (itemCode) => {
+  let stringLessCheckSum = `${manufacturerCode}${itemCode}`;
+
+  let num1 = 0;
+  let num2 = 0;
+
+  for (let i = 0; i < stringLessCheckSum.length; i++) {
+    if (i % 2 == 0) {
+      num1 += parseInt(stringLessCheckSum[i]);
+    } else {
+      num2 += parseInt(stringLessCheckSum[i]);
+    }
+  }
+
+  num1 *= 3;
+  sum = num1 + num2;
+
+  checkSum = 0;
+  while (sum % 10 != 0) {
+    sum += 1;
+    checkSum += 1;
+  }
+
+  return checkSum;
+};
+
+const calculateItemCode = (itemHash) => {
+  let itemCode = "";
+
+  for (let i = 0; i < itemHash.length; i++) {
+    if (!isNaN(itemHash[i])) {
+      itemCode += itemHash[i];
+    }
+
+    if (itemCode.length == 5) {
+      break;
+    }
+  }
+
+  while (itemCode.length < 5) {
+    itemCode += crypto.randomInt(0, 10);
+  }
+
+  return itemCode;
+};
+
+const calculateUPCCode = (itemName) => {
+  let itemHash = crypto.createHash("md5").update(itemName).digest("hex");
+  console.log(itemHash)
+  let itemCode = calculateItemCode(itemHash);
+  let checkSum = calculateCheckSum(itemCode);
+
+  return `${manufacturerCode}${itemCode}${checkSum}`;
+};
 
 const inventorySchema = new mongoose.Schema({
   name: {
@@ -17,4 +75,9 @@ const inventorySchema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model("Inventory", inventorySchema)
+inventorySchema.pre("save", function (next) {
+  this.upc = calculateUPCCode(this.name);
+  next();
+});
+
+module.exports = mongoose.model("Inventory", inventorySchema);
