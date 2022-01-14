31319bc66732500529cab12593c73a110ec7c044
Elias Hawa <elias.hawa2468@gmail.com>
2022-01-14 10:03:25 -0500

    Refactor shipment functions to service file

--- routes/shipment.js
+++ routes/shipment.js
@@ -1,106 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { Shipment, Inventory } = require("../models");
-
-//Verify that products can be added
-const verifyInventory = async (listOfInventory) => {
-  for (var upc in listOfInventory) {
-    const currentItem = await Inventory.findOne({ upc: upc });
-
-    if (!currentItem) {
-      throw new Error(`Could not find product with UPC code: ${upc}`);
-    }
-
-    if (currentItem.amount < parseInt(listOfInventory[upc])) {
-      throw new Error(
-        `Product with UPC code: ${upc} only has ${currentItem.amount} in stock while ${listOfInventory[upc]} was requested`
-      );
-    }
-  }
-
-  return true;
-};
-
-//Verify that new products can be added
-//If they have already been added, verfiy that their new amounts are possible
-const verifyNewInventory = async (listOfInventory, newList) => {
-  for (var upc in newList) {
-    if (listOfInventory.get(upc) !== undefined) {
-      const currentInventory = await Inventory.findOne({ upc: upc });
-      const totalAfterReturn =
-        currentInventory.amount + listOfInventory.get(upc);
-
-      if (totalAfterReturn < parseInt(newList[upc])) {
-        throw new Error(
-          `Product with UPC code: ${upc} only has ${totalAfterReturn} in stock while ${newList[upc]} was requested`
-        );
-      }
-
-    } else {
-      const currentItem = await Inventory.findOne({ upc: upc });
-
-      if (!currentItem) {
-        throw new Error(`Could not find product with UPC code: ${upc}`);
-      }
-
-      if (currentItem.amount < parseInt(newList[upc])) {
-        throw new Error(
-          `Product with UPC code: ${upc} only has ${
-            currentItem.amount
-          } in stock while ${newList[upc]} was requested`
-        );
-      }
-    }
-  }
-
-  return true
-};
-
-const updateInventory = async (listOfInventory) => {
-  for (var upc in listOfInventory) {
-    const currentItem = await Inventory.findOne({ upc: upc });
-    await Inventory.findByIdAndUpdate(currentItem._id, {
-      amount: currentItem.amount - parseInt(listOfInventory[upc]),
-    });
-  }
-};
-
-const updateNewInventory = async (listOfInventory, newList) => {
-  //For each upc code in the new list
-  for (var upc in newList) {
-    //If it existed in the old list
-    if (listOfInventory.get(upc) !== undefined) {
-      //modify it's amount to reflect whats now being used
-      const currentItem = await Inventory.findOne({ upc: upc });
-      await Inventory.findByIdAndUpdate(currentItem._id, {
-        amount: currentItem.amount + listOfInventory.get(upc) - newList[upc],
-      });
-
-      //If it didn't exist in the old list, update normally
-    } else {
-      const currentItem = await Inventory.findOne({ upc: upc });
-      await Inventory.findByIdAndUpdate(currentItem._id, {
-        amount: currentItem.amount - parseInt(newList[upc]),
-      });
-    }
-  }
-
-  //For each upc code in the old list
-  for (const upc of listOfInventory.keys()) {
-    //If the upc doesn't exist in the old list (item was removed from shipment)
-    if (newList[upc] === undefined) {
-      //Add items back to inventory
-      const currentItem = await Inventory.findOne({ upc: upc });
-      await Inventory.findByIdAndUpdate(currentItem._id, {
-        amount: currentItem.amount + listOfInventory.get(upc),
-      });
-    }
-  }
-};
+const shipmentService = require("../services/shipment.service");
 
 router.get("/", async (req, res) => {
   try {
-    const allShipments = await Shipment.find();
+    const allShipments = await shipmentService.getAllShipments();
     res.status(200).send(allShipments);
   } catch (err) {
     res.status(500).json({ message: err.message });
@@ -109,7 +13,7 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-    const shipmentFromId = await Shipment.findById(req.params.id);
+    const shipmentFromId = await shipmentService.getShipmentById(req.params.id);
     res.status(200).send(shipmentFromId);
   } catch (err) {
     res.status(500).json({ message: err.message });
@@ -118,11 +22,7 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", async (req, res) => {
   try {
-    const hasEnoughStock = await verifyInventory(req.body.contents);
-    if (hasEnoughStock) {
-      await updateInventory(req.body.contents);
-    }
-    const newShipment = await Shipment.create(req.body);
+    const newShipment = await shipmentService.createShipment(req.body);
     res.status(200).send(newShipment);
   } catch (err) {
     if (
@@ -139,25 +39,11 @@ router.post("/", async (req, res) => {
 
 router.put("/:id", async (req, res) => {
   try {
-    const shipment = await Shipment.findById(req.params.id);
-    if (!shipment) {
-      throw new Error(`Shipment with id: ${req.params.id} not found`);
-    }
-
-    if (req.body.contents) {
-      const hasEnoughAfterReturns = await verifyNewInventory(
-        shipment.contents,
-        req.body.contents
-      );
-      if (hasEnoughAfterReturns) {
-        await updateNewInventory(shipment.contents, req.body.contents);
-      }
-    }
-
-    await Shipment.findByIdAndUpdate(req.params.id, req.body);
-    Object.assign(shipment, req.body);
-
-    res.status(200).send(shipment);
+    const updatedShipment = await shipmentService.updateShipment(
+      req.params.id,
+      req.body
+    );
+    res.status(200).send(updatedShipment);
   } catch (err) {
     if (
       err.message.startsWith("Shipment with id:") ||
@@ -166,24 +52,22 @@ router.put("/:id", async (req, res) => {
       err.message.startsWith("Product with UPC code:")
     ) {
       res.status(400).send({ message: err.message });
+    } else {
+      res.status(500).send({ message: err.message });
     }
   }
 });
 
 router.delete("/", async (req, res) => {
-  await Shipment.deleteMany();
+  await shipmentService.deleteAllShipments();
   res.status(200).send("All shipments deleted");
 });
 
 router.delete("/:id", async (req, res) => {
   try {
-    const shipmentToDelete = await Shipment.findById(req.params.id);
-
-    if (!shipmentToDelete) {
-      throw new Error(`Shipment with id: ${req.params.id} not found`);
-    }
-
-    await Shipment.findByIdAndDelete(req.params.id);
+    const shipmentToDelete = await shipmentService.deleteShipmentById(
+      req.params.id
+    );
     res
       .status(200)
       .send(`Item with id: ${req.params.id} deleted. \n${shipmentToDelete}`);
---
+++ services/shipment.service.js
@@ -0,0 +1,208 @@
+const { Shipment, Inventory } = require("../models");
+
+/**
+ * Checks current inventory to see if shipment can be made
+ * @param {Map} listOfInventory 
+ * @returns {Boolean} - true if requested inventory is available for shipment, error otherwise 
+ */
+const verifyInventory = async (listOfInventory) => {
+  for (var upc in listOfInventory) {
+    const currentItem = await Inventory.findOne({ upc: upc });
+
+    if (!currentItem) {
+      throw new Error(`Could not find product with UPC code: ${upc}`);
+    }
+
+    if (currentItem.amount < parseInt(listOfInventory[upc])) {
+      throw new Error(
+        `Product with UPC code: ${upc} only has ${currentItem.amount} in stock while ${listOfInventory[upc]} was requested`
+      );
+    }
+  }
+
+  return true;
+};
+
+/**
+ * Checks inventory to see if shipment can be made after possibly returning inventory from old list of inventory
+ * @param {Map} listOfInventory 
+ * @param {Map} newList 
+ * @returns {Boolean} - true if requested inventory update can be made after returning inventory in current shipment, error otherwise
+ */
+const verifyInventoryUpdate = async (listOfInventory, newList) => {
+  for (var upc in newList) {
+    if (listOfInventory.get(upc) !== undefined) {
+      const currentInventory = await Inventory.findOne({ upc: upc });
+      const totalAfterReturn =
+        currentInventory.amount + listOfInventory.get(upc);
+
+      if (totalAfterReturn < parseInt(newList[upc])) {
+        throw new Error(
+          `Product with UPC code: ${upc} only has ${totalAfterReturn} in stock while ${newList[upc]} was requested`
+        );
+      }
+    } else {
+      const currentItem = await Inventory.findOne({ upc: upc });
+
+      if (!currentItem) {
+        throw new Error(`Could not find product with UPC code: ${upc}`);
+      }
+
+      if (currentItem.amount < parseInt(newList[upc])) {
+        throw new Error(
+          `Product with UPC code: ${upc} only has ${currentItem.amount} in stock while ${newList[upc]} was requested`
+        );
+      }
+    }
+  }
+
+  return true;
+};
+
+/**
+ * Update inventory after shipment is made
+ * @param {Map} listOfInventory 
+ */
+const updateInventory = async (listOfInventory) => {
+  for (var upc in listOfInventory) {
+    const currentItem = await Inventory.findOne({ upc: upc });
+    await Inventory.findByIdAndUpdate(currentItem._id, {
+      amount: currentItem.amount - parseInt(listOfInventory[upc]),
+    });
+  }
+};
+
+/**
+ * Update inventory after shipment update is made
+ * @param {Map} listOfInventory 
+ * @param {Map} newList 
+ */
+const updateNewInventory = async (listOfInventory, newList) => {
+  //For each upc code in the new list
+  for (var upc in newList) {
+    //If it existed in the old list
+    if (listOfInventory.get(upc) !== undefined) {
+      //modify it's amount to reflect whats now being used
+      const currentItem = await Inventory.findOne({ upc: upc });
+      await Inventory.findByIdAndUpdate(currentItem._id, {
+        amount: currentItem.amount + listOfInventory.get(upc) - newList[upc],
+      });
+
+      //If it didn't exist in the old list, update normally
+    } else {
+      const currentItem = await Inventory.findOne({ upc: upc });
+      await Inventory.findByIdAndUpdate(currentItem._id, {
+        amount: currentItem.amount - parseInt(newList[upc]),
+      });
+    }
+  }
+
+  //For each upc code in the old list
+  for (const upc of listOfInventory.keys()) {
+    //If the upc doesn't exist in the old list (item was removed from shipment)
+    if (newList[upc] === undefined) {
+      //Add items back to inventory
+      const currentItem = await Inventory.findOne({ upc: upc });
+      await Inventory.findByIdAndUpdate(currentItem._id, {
+        amount: currentItem.amount + listOfInventory.get(upc),
+      });
+    }
+  }
+};
+
+/**
+ * Get all shipments
+ * @returns {Promise<Shipment>}
+ */
+const getAllShipments = async () => {
+  const allShipments = await Shipment.find();
+  return allShipments;
+};
+
+/**
+ * Get shipment by id
+ * @param {ObjectId} id 
+ * @returns {Promise<Shipment>}
+ */
+const getShipmentById = async (id) => {
+  const shipmentFromId = await Shipment.findById(id);
+  return shipmentFromId;
+};
+
+/**
+ * Create shipment
+ * @param {Object} body 
+ * @returns {Promise<Shipment>}
+ */
+const createShipment = async (body) => {
+  const hasEnoughStock = await verifyInventory(body.contents);
+  if (hasEnoughStock) {
+    await updateInventory(body.contents);
+  }
+  const newShipment = await Shipment.create(body);
+  return newShipment;
+};
+
+/**
+ * Update shipment by id
+ * @param {ObjectId} id 
+ * @param {Object} updateBody 
+ * @returns {Promise<Shipment>}
+ */
+const updateShipment = async (id, updateBody) => {
+  const shipment = await Shipment.findById(id);
+  if (!shipment) {
+    throw new Error(`Shipment with id: ${id} not found`);
+  }
+
+  if (updateBody.contents) {
+    const hasEnoughAfterReturns = await verifyInventoryUpdate(
+      shipment.contents,
+      updateBody.contents
+    );
+    if (hasEnoughAfterReturns) {
+      await updateNewInventory(shipment.contents, updateBody.contents);
+    }
+  }
+
+  await Shipment.findByIdAndUpdate(id, updateBody);
+  Object.assign(shipment, updateBody);
+
+  return shipment;
+};
+
+/**
+ * Delete all shipments
+ */
+const deleteAllShipments = async () => {
+  await Shipment.deleteMany();
+};
+
+/**
+ * Delete shipment by id
+ * @param {ObjectId} id 
+ * @returns 
+ */
+const deleteShipmentById = async (id) => {
+  const shipmentToDelete = await Shipment.findById(id);
+
+  if (!shipmentToDelete) {
+    throw new Error(`Shipment with id: ${id} not found`);
+  }
+
+  await Shipment.findByIdAndDelete(id);
+  return shipmentToDelete;
+};
+
+module.exports = {
+  verifyInventory,
+  verifyInventoryUpdate,
+  updateInventory,
+  updateNewInventory,
+  getAllShipments,
+  getShipmentById,
+  createShipment,
+  updateShipment,
+  deleteAllShipments,
+  deleteShipmentById,
+};
